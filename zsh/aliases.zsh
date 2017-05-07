c82b6f28cc779496588b5867121608c47a6e36d6
Chris Hicks <chicks@flow.io>
2017-05-07 08:03:00 -0400

    revamped dotfiles

---
+++ zsh/aliases.zsh
@@ -0,0 +1,3 @@
+alias ls='ls -aG'
+alias ll='ls -laG'
+alias reload='source /Users/chicks/.zshrc'
---
+++ zsh/path.zsh
@@ -0,0 +1,3 @@
+export PATH=$PATH:~/bin
+export PATH="$(brew --prefix git)/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$PATH"
