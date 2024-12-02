6354b133a03ce7725a492ecd7e73d78d108b119e
Christopher Hicks <chrhicks@gmail.com>
2024-12-02 09:23:06 -0500

    fix: #6  remove junk, use new dir structure

--- zsh/aliases.zsh
+++
@@ -1,4 +0,0 @@
-alias ls='ls -aG'
-alias ll='ls -laG'
-alias reload='source ~/.zshrc'
-alias backup='mkdir -p ~/.backup && cp -R ~/.dotfiles ~/.apibuilder ~/.zsh_history ~/.aws ~/.chicksrc ~/.flow ~/.gnupg ~/.npmrc ~/.oh-my-zsh ~/.ssh ~/.tools ~/.backup'
